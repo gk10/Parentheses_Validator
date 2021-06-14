15b7959e2205ab011bed5277fe332c2003a9cca0
Greg Kline <gkline@stetson.edu>
2021-06-14 09:23:13 -0400

    Code + file for parentheses validator

---
+++ src/Customers.java
@@ -0,0 +1,46 @@
+
+public class Customers {
+	private String id, fname, lname, company;
+	private int version;
+	
+	public String getID() {
+		return id;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	
+	public String getLname() {
+		return lname;
+	}
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public int getVersion() {
+		return version;
+	}
+	
+	public void setID(String a) {
+		id = a;
+	}
+	
+	public void setFname(String a) {
+		fname = a;
+	}
+	
+	public void setlName(String a) {
+		lname = a;
+	}
+	
+	public void setCompany(String a) {
+		company = a;
+	}
+	
+	public void setVersion(int a) {
+		version = a;
+	}
+	
+}
